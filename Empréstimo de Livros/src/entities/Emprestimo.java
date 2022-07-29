7d97c4c332d568576ea0f900bb08aec0db2f2b96
Gabriel Gomes <gabriel.gomes2022@outlook.com>
2022-07-29 17:11:39 -0300

    Update Emprestimo.java

--- Empréstimo de Livros/src/entities/Emprestimo.java
+++ Empréstimo de Livros/src/entities/Emprestimo.java
@@ -11,51 +11,30 @@ public class Emprestimo {
     private Date devolucaoPrevista;
     private Date devolucaoReal;
 
-    /**
-     * @return int return the idEmprestimo
-     */
     public int getIdEmprestimo() {
         return idEmprestimo;
     }
 
-    /**
-     * @param idEmprestimo the idEmprestimo to set
-     */
     public void setIdEmprestimo(int idEmprestimo) {
         this.idEmprestimo = idEmprestimo;
     }
 
-    /**
-     * @return Usuario return the usuario
-     */
     public int getIdUsuario() {
         return idUsuario;
     }
 
-    /**
-     * @param usuario the usuario to set
-     */
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
 
-    /**
-     * @return Livro return the livro
-     */
     public int getLivro() {
         return idLivro;
     }
 
-    /**
-     * @param livro the livro to set
-     */
     public void setIdLivro(int idLivro) {
         this.idLivro = idLivro;
     }
 
-    /**
-     * @return Date return the dataRetirada
-     */
     public Date getDataRetirada() {
         return dataRetirada;
     }
